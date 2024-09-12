fe8cd7075be7e7c4f0460b80669bfe00db7ffa76
Mauricio Oliveda <maurioliveda@gmail.com>
2024-09-12 11:22:19 -0300

    :lipstick: Add design style.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 const config = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        orange: {
+          65: "#FF8D4D",
+          70: "#FF9E66",
+          75: "#FFAE80",
+          80: "#FFBE99",
+          90: "#FFDECC",
+          95: "#FFEFE5",
+          97: "#FFEFE5",
+          99: "#FFFCFA",
+        },
+        grey: {
+          10: "#1A1A1A",
+          15: "#262626",
+          20: "#333333",
+          30: "#4C4C4D",
+          35: "#59595A",
+          40: "#656567",
+          60: "#98989A",
+          70: "#B3B3B3",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-outfit)"],
+        headings: ["var(--font-raleway)"],
+      },
+    },
   },
   plugins: [],
 };
