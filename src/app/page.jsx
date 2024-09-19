2d2e050bdb68869e97e6b48f06b8761fe4b9d61a
Mauricio Oliveda <maurioliveda@gmail.com>
2024-09-19 16:34:22 -0300

    :construction: Implement webiste header.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/Banner";
+import WebsiteHeader from "@/components/WebsiteHeader";
 
 export default function HomePage() {
   return (
     <>
       <Banner text="Admission is Open, Grab your seat now" />
+      <WebsiteHeader />
     </>
   );
 }
