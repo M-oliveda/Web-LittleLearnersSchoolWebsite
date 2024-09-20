daf0bbbd4842b81c677fb5ab8e73f58732cbd8bc
Mauricio Oliveda <maurioliveda@gmail.com>
2024-09-20 15:24:00 -0300

    :construction: Add homepage benefits section.
    This commits adds the benefits section into homepage.
    And also, adds a layout for **main content.**

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,17 @@
 import Banner from "@/components/Banner";
 import WebsiteHeader from "@/components/WebsiteHeader";
 import HeroSection from "./layout/HeroSection";
+import BenefitsSection from "./layout/BenefitsSection";
 
 export default function HomePage() {
   return (
     <>
       <Banner text="Admission is Open, Grab your seat now" />
       <WebsiteHeader />
-      <HeroSection />
+      <main className="mx-auto xl:w-[80%]">
+        <HeroSection />
+        <BenefitsSection />
+      </main>
     </>
   );
 }
