39d14e42ac55bdc1827d36e3f4d1a1591c35c018
Mauricio Oliveda <maurioliveda@gmail.com>
2024-09-24 09:22:04 -0300

    :recycle: Improve website hero section.

--- src/app/academics/page.jsx
+++ src/app/academics/page.jsx
@@ -1,10 +1,20 @@
+import WebsiteHero from "@/components/WebsiteHero";
 import AcademicsFeatures from "./layout/AcademicsFeaturesSection";
-import HeroSection from "./layout/HeroSection";
 
 export default function AcademicsPage() {
   return (
     <main>
-      <HeroSection />
+      <WebsiteHero
+        message="Academics"
+        title="Nurturing Young Minds for Success"
+        paragraph="Welcome to our Academics page, where we take pride in providing a
+        comprehensive and stimulating educational experience for your child. Our
+        kindergarten school's academic program is thoughtfully designed to
+        foster a love for learning while building a strong foundation of
+        essential skills and knowledge. From language arts and mathematics to
+        science and social studies, our curriculum is carefully crafted to spark
+        curiosity and encourage active exploration."
+      />
       <AcademicsFeatures />
     </main>
   );
