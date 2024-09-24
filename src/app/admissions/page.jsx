fc975f8a3ab959efeda0e9db9350d1bdaf9cb6f2
Mauricio Oliveda <maurioliveda@gmail.com>
2024-09-24 17:51:21 -0300

    :construction: Add admissions page.

---
+++ src/app/admissions/page.jsx
@@ -0,0 +1,13 @@
+import WebsiteHero from "@/components/WebsiteHero";
+
+export default function AdmissionsPage() {
+  return (
+    <main>
+      <WebsiteHero
+        message="Admission"
+        title="Join Our Family of Young Learners"
+        paragraph="At Little Learners Academy, we welcome you to embark on an exciting educational journey for your child. Our admission process is designed to be transparent, straightforward, and inclusive. Here's a step-by-step guide to joining our school"
+      />
+    </main>
+  );
+}
