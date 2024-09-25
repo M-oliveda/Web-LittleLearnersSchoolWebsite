6c8fb52e91d6893e4df4ad1bed6005acfa520016
Mauricio Oliveda <maurioliveda@gmail.com>
2024-09-25 11:16:38 -0300

    :construction: Add admissions steps section.

--- src/app/admissions/page.jsx
+++ src/app/admissions/page.jsx
@@ -1,4 +1,6 @@
 import WebsiteHero from "@/components/WebsiteHero";
+import WebsiteSection from "@/components/WebsiteSection";
+import AdmissionSteps from "./components/AdmissionSteps";
 
 export default function AdmissionsPage() {
   return (
@@ -8,6 +10,14 @@ export default function AdmissionsPage() {
         title="Join Our Family of Young Learners"
         paragraph="At Little Learners Academy, we welcome you to embark on an exciting educational journey for your child. Our admission process is designed to be transparent, straightforward, and inclusive. Here's a step-by-step guide to joining our school"
       />
+      <WebsiteSection
+        id="steps"
+        message="Process"
+        title="Admission Process"
+        paragraph="Embark on a remarkable educational journey with us! Our Admission and Enrollment process is the gateway to providing your child with an exceptional learning experience at our kindergarten school"
+      >
+        <AdmissionSteps />
+      </WebsiteSection>
     </main>
   );
 }
